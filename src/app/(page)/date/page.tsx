4f284359bc141d1f1d2c9e85c280990da005682d
Rika-L <2209393334@qq.com>
2025-04-11 20:28:18 +0800

    feat: date

--- src/app/(page)/date/page.tsx
+++ src/app/(page)/date/page.tsx
@@ -1,11 +1,12 @@
 'use client'
 
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
-import { Card } from '@/components/ui/card'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import DisplayCards from '@/components/ui/display-cards'
 import { useDate } from '@/hooks/useDate'
 import { getAnniversaryCard } from '@/utils/getAnniversaryCard'
 import { Terminal } from 'lucide-react'
+import moment from 'moment'
 
 function Page() {
   const { data, error, isLoading } = useDate()
@@ -38,7 +39,20 @@ function Page() {
       <div className="columns-3 gap-8 space-x-8 space-y-8">
         {defaultDate.map(item => {
           return (
-            <Card key={item.id} className="overflow-hidden break-inside-avoid">{JSON.stringify(item)}</Card>)
+            <Card
+              key={item.id}
+              className="break-inside-avoid relative after:content-[''] after:absolute
+              after:size-full after:top-0 after:opacity-0  after:blur-2xl hover:after:opacity-50 after:-z-10
+              after:transition-all after:duration-500 after:from-[#7a23a1] after:via-[#715ebde6] after:to-[#bd34fe70] after:bg-gradient-to-br
+            "
+            >
+              <CardHeader>
+                <CardTitle>{item.title}</CardTitle>
+                <CardDescription>{moment(item.timestamp).format('YYYY-MM-DD hh:mm:ss')}</CardDescription>
+              </CardHeader>
+              <CardContent>{item.description}</CardContent>
+            </Card>
+          )
         })}
       </div>
     </div>
--- src/components/glow.tsx
+++ src/components/glow.tsx
@@ -11,7 +11,7 @@ function Glow() {
     <Fragment>
       <motion.div
         suppressHydrationWarning
-        className="-z-10 fixed bottom-[-30%] left-[-5%] cursor-none pointer-events-none rounded-[100%] w-6xl aspect-[1.5] from-[#7a23a1] via-[#715ebde6] to-[#bd34fe00] bg-gradient-to-br blur-[15vh]"
+        className="-z-10 fixed bottom-[-30%] left-[-5%] cursor-none pointer-events-none rounded-[100%] w-6xl xl:w-7xl aspect-[1.5] from-[#7a23a1] via-[#715ebde6] to-[#bd34fe00] bg-gradient-to-br blur-[15vh]"
         animate={{
           opacity: 0.6,
           left: '-5%',
@@ -26,7 +26,7 @@ function Glow() {
       />
       <motion.div
         suppressHydrationWarning
-        className="-z-10 opacity-60 fixed bottom-[-30%] right-[-5%] cursor-none pointer-events-none rounded-[100%] w-4xl aspect-[1.5] from-[#61d9ff] to-[#0000] bg-gradient-to-b blur-[15vh]"
+        className="-z-10 opacity-60 fixed bottom-[-30%] right-[-5%] cursor-none pointer-events-none rounded-[100%] w-4xl xl:w-5xl aspect-[1.5] from-[#61d9ff] to-[#0000] bg-gradient-to-b blur-[15vh]"
         animate={{
           opacity: 0.6,
           right: '-5%',
